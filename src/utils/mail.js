77a07c4fd43134b15c0eb200562da1a5b964b472
YashChoudam <yashchoudam527@gmail.com>
2025-10-09 00:08:53 +0530

    Mails

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -16,12 +16,12 @@ const sendEmail = async (options) => {
   const emailHtml = mailGenerator.generate(options.mailgenConent);
 
   const transporter = nodemailer.createTransport({
-    host: process.env.MAILTRAP_SMTP_HOST,
+    host: process.env.MAILTRAP_SMTP_HOST, 
     port: process.env.MAILTRAP_SMTP_PORT,
     auth: {
       user: process.env.MAILTRAP_SMTP_USER,
       pass: process.env.MAILTRAP_SMTP_PASS,
-    },
+    },  
   });
 
   const mail = {
